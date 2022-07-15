130f64691988a856123e7f9e3f6d1299b5d5754c
shaulalambdascorpii <105778233+shaulalambdascorpii@users.noreply.github.com>
2022-07-15 15:46:56 +0100

    working on css

--- main.js
+++ main.js
@@ -3,17 +3,21 @@ function myFunction() {
     let randomNumber = Math.floor(Math.random() * 8);
 
     if (randomNumber === 0) {
-      eightBall = "It is certain";
+      eightBall = "It is certain to happen";
     } else if (randomNumber === 1) {
       eightBall="It is decidely so";
     } else if (randomNumber === 2) {
-      eightBall = "so what";
+      eightBall = "It will never happen";
     } else if (randomNumber === 3) {
-      eightBall = "It is ultimately so";
+      eightBall = "It will happen in the next 5 years";
+    } else if (randomNumber === 4) {
+        eightBall = "It will happen in the next 20 years";
+    } else if (randomNumber === 5) {
+        eightBall = "It will happen in the next year";
     } else {
-      eightBall = "it was a high number";
+      eightBall = "It will happen in 50 years";
     }
 
-document.getElementById("demo").innerHTML("hello");
+document.getElementById("demo").innerHTML=(eightBall);
 
 };
